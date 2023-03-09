4b88a7ccc973c6beda807f6b379bdfdeb5f6f1c5
Ashton <85352344+Web-chatsapp@users.noreply.github.com>
2023-03-09 16:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Blue = require("./lib/Blue.js");
-const BlueplayerManager = require("./lib/BluePlayerManager.js");
-const BlueNode = require("./lib/BlueNode.js");
 module.exports = {
-  Blue,
-  BlueplayerManager,
-  BlueNode
+  Blue
 }
 
 
