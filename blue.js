4ad34eaf6f052fbe5317dc4995a5935d8ab859a9
Ashton <85352344+Web-chatsapp@users.noreply.github.com>
2023-03-13 19:29:05 +0530

    Add files via upload

---
+++ blue.js
@@ -0,0 +1,18 @@
+const Blue = require("./lib/Blue.js");
+const Node = require("./lib/node/BlueNode.js");
+const Player = require ("./lib/player/BluePlayer.js")
+
+module.exports = {
+  Blue, 
+  Node, 
+  Player
+}
+
+
+/* 
+* Blue,
+* A Lavalink Client,
+* Made,
+* By,
+* Ashton#4218
+*/
